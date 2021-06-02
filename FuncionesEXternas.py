c0e2eddae6531f63abf251043bd75fa7b7d6beda
Raul Jesus Palma Castilla <70992470+rpalmac@users.noreply.github.com>
2021-06-01 21:08:22 -0500

    Create FuncionesEXternas.py

---
+++ FuncionesEXternas.py
@@ -0,0 +1,14 @@
+#Declaracion de funciones
+
+#Funcion sin parametros
+
+def funcion1 ():
+	print('Has llamado a la funcion 1')
+
+#Funcion con parametros y retorno
+
+def funcion2(a,b):
+	#contenido de la funcion
+        #Esto realiza la suma de esta función
+	c=int(a)+int(b)
+	return c
